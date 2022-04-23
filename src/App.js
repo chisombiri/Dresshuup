fb360e52e4f702f2bf5afb893c54bbd68c5bbdd9
chisombiri <chisombiri@gmail.com>
2022-04-23 01:27:58 +0100

    category styling

--- src/App.js
+++ src/App.js
@@ -1,42 +1,8 @@
-import './categories.scss';
+import Categories from './components/Categories/Categories';
 
 const App = () => {
-
-  const categories = [
-    {
-      id: 1,
-      title: 'Trainers'
-    },
-    {
-      id: 2,
-      title: 'Jackets'
-    },
-    {
-      id: 3,
-      title: 'Hats'
-    },
-    {
-      id: 4,
-      title: 'Men\'s'
-    },
-    {
-      id: 5,
-      title: 'Women\'s'
-    },
-  ]
-
   return (
-    <div className="categories-container">
-      {categories.map(({id, title}) => (
-        <div key={id} className="category-container">
-          <div className="background-image" />
-          <div className="category-body">
-            <h2>{title}</h2>
-            <p>Shop Now!</p>
-          </div>
-        </div>
-      ))}
-    </div>
+    <Categories />
   );
 }
 
---
+++ src/components/CategoryItem/CategoryItem.js
@@ -0,0 +1,22 @@
+import './CategoryItem.scss';
+
+const CategoryItem = ({ category }) => {
+
+    const { title, imageUrl } = category;
+
+    return(
+        <div className="category-container">
+          <div 
+          className="background-image" 
+          style={{
+            backgroundImage: `url(${imageUrl})`
+          }} />
+          <div className="category-body">
+            <h2>{title}</h2>
+            <p>Shop Now!</p>
+          </div>
+        </div>
+    )
+}
+
+export default CategoryItem;
