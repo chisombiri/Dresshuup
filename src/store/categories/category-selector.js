0998f362b32a3d8b5a61b316441fc924a6a9ae63
chisombiri <chisombiri@gmail.com>
2022-05-07 13:32:48 +0100

    implemented create selector on category selector

--- src/store/categories/category-selector.js
+++ src/store/categories/category-selector.js
@@ -1,6 +1,18 @@
-export const selectCategoriesMap = (state) => state.categories.categories
-.reduce((acc, category) => {
-    const { title, items } = category;
-    acc[title.toLowerCase()] = items;
-    return acc;
-  }, {});
+import { createSelector } from "reselect";
+
+const selectCategoryReducer = (state) => state.categories;
+
+export const selectCategories = createSelector(
+    [selectCategoryReducer], //input selectors
+    (categoriesSlice) => categoriesSlice.categories //output selectors
+);
+
+//using create selector so as long as categories array remains same we don't re run the method and just get previous value
+export const selectCategoriesMap = createSelector(
+    [selectCategories],
+    (categories) => categories.reduce((acc, category) => {
+        const { title, items } = category;
+        acc[title.toLowerCase()] = items;
+        return acc;
+        }, {})
+);
