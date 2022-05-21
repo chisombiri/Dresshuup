b3a84aeb63308d80d4b9d16931543af25e3e130f
chisombiri <chisombiri@gmail.com>
2022-05-22 00:05:14 +0100

    inculded redux-persist to persist data to local storage

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-
 import { Provider } from 'react-redux';
-import { store } from './store/store';
+import { PersistGate } from 'redux-persist/integration/react';
+
+import { store, persistor } from './store/store';
 
 import './index.scss';
 import App from './App';
@@ -13,9 +14,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-          <App />
-      </BrowserRouter>
+      <PersistGate persistor={persistor}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/store/store.js
+++ src/store/store.js
@@ -3,11 +3,26 @@
 import { configureStore } from "@reduxjs/toolkit"
 import  { logger } from "redux-logger";
 
+//importing reducer and store methods from redux persist
+import { persistReducer, persistStore } from "redux-persist";
+import storage from 'redux-persist/lib/storage';
+
 //root reducer: combination of all reducers
 import { rootReducer } from "./root-reducer";
 
-export const store = configureStore({
-    reducer: rootReducer, 
-    middleware: [logger]
-});
+const persistConfig = {
+    key: 'root',
+    storage,
+    blacklist: ['user'] //user won't be persisted to avoid conflict with authentication
+}
 
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = configureStore(
+    {
+    reducer: persistedReducer, 
+    middleware: [logger]
+    }
+);
+
+export const persistor = persistStore(store);
