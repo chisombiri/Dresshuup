221f8e434bc0fe3dba7e7409a5c68d68745249f1
chisombiri <chisombiri@gmail.com>
2022-05-21 23:32:05 +0100

    cart migration complete, deleted cart context

--- src/components/Checkout-Item/Checkout-Item.js
+++ src/components/Checkout-Item/Checkout-Item.js
@@ -1,15 +1,21 @@
-import { useContext } from "react";
-import { CartContext } from "../../contexts/cart-context";
+// import { useContext } from "react";
+// import { CartContext } from "../../contexts/cart-context";
+
+import { useDispatch, useSelector } from "react-redux";
+
+import { selectCartItems } from "../../store/cart/cart-selector"
+import { clearItemFromCart, removeItemFromCart, addItemToCart } from "../../store/cart/cart-action"
 import "./checkout-item.scss";
 
 const CheckoutItem = ({ cartItem }) => {
     const { name, imageUrl, price, quantity } = cartItem;
-    const { clearItemFromCart, addItemToCart, removeItemFromCart } = useContext(CartContext);
+    const cartItems = useSelector(selectCartItems);
+    const dispatch = useDispatch();
 
     //event handlers
-    const clearItemHandler = () => clearItemFromCart(cartItem);
-    const addItemHandler = () => addItemToCart(cartItem);
-    const removeItemHandler = () => removeItemFromCart(cartItem);
+    const clearItemHandler = () => dispatch(clearItemFromCart(cartItems, cartItem));
+    const addItemHandler = () => dispatch(addItemToCart(cartItems, cartItem));
+    const removeItemHandler = () => dispatch(removeItemFromCart(cartItems, cartItem));
 
     return(
         <div className="checkout-item-container">
--- src/routes/Checkout/Checkout.js
+++ src/routes/Checkout/Checkout.js
@@ -1,10 +1,14 @@
-import { useContext } from "react";
-import { CartContext } from "../../contexts/cart-context";
+// import { useContext } from "react";
+// import { CartContext } from "../../contexts/cart-context";
+import { useSelector } from "react-redux";
+import { selectCartItems, selectCartTotal } from "../../store/cart/cart-selector"
 import CheckoutItem from "../../components/Checkout-Item/Checkout-Item";
 import "./checkout.scss";
 
 const Checkout = () => {
-    const { cartItems, cartTotal } = useContext(CartContext);
+    const cartItems = useSelector(selectCartItems);
+    const cartTotal = useSelector(selectCartTotal);
+    
     return(
         <div className="checkout-container">
             <div className="checkout-header">
