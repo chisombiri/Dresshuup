bb895b48dfcec81a6469aa5a6f8b1ff88f243d33
chisombiri <chisombiri@gmail.com>
2022-09-25 00:37:56 +0100

    typed payment form

--- src/components/payment-form/PaymentForm.tsx
+++ src/components/payment-form/PaymentForm.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, FormEvent } from "react";
 import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
 import { useSelector } from "react-redux";
 import { selectCartTotal } from "../../store/cart/cart-selector";
@@ -19,7 +19,7 @@ const PaymentForm = () => {
   const currentUser = useSelector(selectCurrentUser);
   const [isProcessingPayment, setIsProcessingPayment] = useState(false);
 
-  const paymentHandler = async (e) => {
+  const paymentHandler = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     if (!stripe || !elements) {
@@ -40,9 +40,13 @@ const PaymentForm = () => {
       paymentIntent: { client_secret },
     } = response;
 
+    const cardDetails = elements.getElement(CardElement);
+
+    if (cardDetails === null) return;
+
     const paymentResult = await stripe.confirmCardPayment(client_secret, {
       payment_method: {
-        card: elements.getElement(CardElement),
+        card: cardDetails,
         billing_details: {
           name: currentUser ? currentUser.displayName : "Guest",
         },
---
+++ src/routes/Category/Category.tsx
@@ -0,0 +1,46 @@
+import { useState, useEffect, Fragment } from "react";
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import {
+  selectCategoriesIsLoading,
+  selectCategoriesMap,
+} from "../../store/categories/category-selector";
+import Spinner from "../../components/Spinner/Spinner";
+
+import ProductCard from "../../components/ProductCard/ProductCard";
+import "./category.scss";
+
+type CategoryRouteParams = {
+  category: string;
+};
+
+const Category = () => {
+  const { category } = useParams<
+    keyof CategoryRouteParams
+  >() as CategoryRouteParams;
+  const categoriesMap = useSelector(selectCategoriesMap);
+  const isLoading = useSelector(selectCategoriesIsLoading);
+  const [products, setProducts] = useState(categoriesMap[category]);
+
+  useEffect(() => {
+    setProducts(categoriesMap[category]);
+  }, [category, categoriesMap]);
+
+  return (
+    <Fragment>
+      <h2 className="category-title">{category.toUpperCase()}</h2>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <div className="category-container">
+          {products &&
+            products.map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))}
+        </div>
+      )}
+    </Fragment>
+  );
+};
+
+export default Category;
