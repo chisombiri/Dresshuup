399129ab81a46593a87c6da296d5d7314bf41e2c
chisombiri <chisombiri@gmail.com>
2022-09-26 23:25:40 +0100

    code splitting with dynamic imports

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import { Route, Routes } from "react-router-dom";
 
-import { useEffect } from "react";
+import { useEffect, lazy, Suspense } from "react";
 import { useDispatch } from "react-redux";
 
 import { checkUserSession } from "./store/user/user-action";
 
-import Navigation from "./routes/Navigation/Navigation";
-import Home from "./routes/Home/Home";
-import Checkout from "./routes/Checkout/Checkout";
-import Authentication from "./routes/Authentication/Authentication";
-import Shop from "./routes/Shop/Shop";
+//import Navigation from "./routes/Navigation/Navigation";
+//import Home from "./routes/Home/Home";
+//import Checkout from "./routes/Checkout/Checkout";
+//import Authentication from "./routes/Authentication/Authentication";
+//import Shop from "./routes/Shop/Shop";
+import Spinner from "./components/Spinner/Spinner";
+
+//lazy dynamic import. Only pulls homepage when required
+const Home = lazy(() => import("./routes/Home/Home"));
+const Authentication = lazy(() =>
+  import("./routes/Authentication/Authentication")
+);
+const Navigation = lazy(() => import("./routes/Navigation/Navigation"));
+const Shop = lazy(() => import("./routes/Shop/Shop"));
+const Checkout = lazy(() => import("./routes/Checkout/Checkout"));
 
 const App = () => {
   const dispatch = useDispatch();
@@ -20,14 +30,16 @@ const App = () => {
   }, [dispatch]);
 
   return (
-    <Routes>
-      <Route path="/" element={<Navigation />}>
-        <Route index element={<Home />} />
-        <Route path="shop/*" element={<Shop />} /> {/* Nested route */}
-        <Route path="auth" element={<Authentication />} />
-        <Route path="checkout" element={<Checkout />} />
-      </Route>
-    </Routes>
+    <Suspense fallback={<Spinner />}>
+      <Routes>
+        <Route path="/" element={<Navigation />}>
+          <Route index element={<Home />} />
+          <Route path="shop/*" element={<Shop />} /> {/* Nested route */}
+          <Route path="auth" element={<Authentication />} />
+          <Route path="checkout" element={<Checkout />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 };
 
