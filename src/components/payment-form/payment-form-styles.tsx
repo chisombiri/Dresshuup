1b436d5d3d053a44a3545e3948a4a0278360c741
chisombiri <chisombiri@gmail.com>
2022-09-27 15:47:15 +0100

    checkout mobile responsiveness

--- src/components/payment-form/payment-form-styles.tsx
+++ src/components/payment-form/payment-form-styles.tsx
@@ -7,11 +7,19 @@ export const PaymentFormContainer = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
+
+  @media screen and (max-width: 800px) {
+    width: 50%;
+  }
 `;
 
 export const FormContainer = styled.form`
   height: 100px;
   min-width: 500px;
+
+  @media screen and (max-width: 800px) {
+    width: 50%;
+  }
 `;
 
 export const PaymentButton = styled(Button)`
